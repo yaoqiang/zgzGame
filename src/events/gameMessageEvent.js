40de43a058644594c4b6083ac7cbadd86853f29c
yaoqiang <bob.yao@kanche.com>
2015-05-30 00:52:42 +0800

    defined some event handle, and remove gameActionEvent.js

--- src/events/gameMessageEvent.js
+++ src/events/gameMessageEvent.js
@@ -4,9 +4,46 @@ pomelo.on('onJoin', function (data) {
     console.log('receive join event.');
     console.log(data);
 
-    cc.eventManager.dispatchCustomEvent("join", data);
+    cc.eventManager.dispatchCustomEvent("onJoin", data);
 
 });
+
+pomelo.on('onLeave', function () {
+
+});
+
+pomelo.on('onReady', function () {
+
+});
+
+pomelo.on('onTalk', function () {
+
+});
+
+pomelo.on('onGameStart', function () {
+
+});
+
+pomelo.on('onDeal', function () {
+
+});
+
+pomelo.on('onFanOut', function () {
+
+});
+
+pomelo.on('onChat', function () {
+
+});
+
+pomelo.on('onBroadcast', function () {
+
+});
+
+pomelo.on('onGoldChange', function (data) {
+
+});
+
 
 pomelo.on('disconnect', function(reason) {
     console.log('disconnected..');
