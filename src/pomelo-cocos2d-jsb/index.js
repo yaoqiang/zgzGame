fb37f0b6adf20ffdbc5ce1eee34efb9aa19a117f
yaoqiang <bob.yao@kanche.com>
2015-05-30 11:13:00 +0800

    add pomelo lib

---
+++ src/pomelo-cocos2d-jsb/index.js
@@ -0,0 +1,33 @@
+var window = window || {};
+var module = module || {};
+module.exports = {};
+
+var console = cc;
+console.error = cc.log;
+
+var setTimeout = function(fn, interval) {
+	var instance = cc.Director.getInstance();
+	var scene = instance.getRunningScene();
+
+	instance.getScheduler().
+		scheduleCallbackForTarget(scene, fn, interval, 1, 0, false);
+	return scene;
+}
+
+var clearTimeout = function(target) {
+	var instance = cc.Director.getInstance();
+	instance.getScheduler().unscheduleAllCallbacksForTarget(target);
+}
+
+window.setTimeout = setTimeout;
+window.clearTimeout = clearTimeout;
+
+require('src/pomelo-cocos2d-jsb/lib/emitter/index.js');
+
+window.EventEmitter = Emitter;
+
+require('src/pomelo-cocos2d-jsb/lib/pomelo-protocol/lib/protocol.js');
+
+require('src/pomelo-cocos2d-jsb/lib/pomelo-protobuf/lib/client/protobuf.js');
+
+require('src/pomelo-cocos2d-jsb/lib/pomelo-jsclient-websocket/lib/pomelo-client.js');
