57a93faf56366f525f81f70ce6cbf58dfc368865
yaoqiang <yaoqiangshmily@gmail.com>
2016-01-04 21:40:54 +0800

    注释提示信息

--- src/events/baseEvent.js
+++ src/events/baseEvent.js
@@ -5,10 +5,11 @@ cc.eventManager.addCustomListener(cc.game.EVENT_HIDE, function(){
 });
 cc.eventManager.addCustomListener(cc.game.EVENT_SHOW, function(){
     cc.log("重新返回游戏");
-    //如果网络与服务器连接已断开, 则自动登录
+    //如果网络与服务器连接已断开, 则自动登录, 目前处理方式有问题,
+    // gNetworkState会在连接GameServer前, 断开gate server时被设置为1;
     if (gNetworkState == 1) {
         //TODO 暂时这么处理
-        prompt.fadeMiddle("您已断开连接, 请重新登录");
+        //prompt.fadeMiddle("您已断开连接, 请重新登录");
     }
 });
 
