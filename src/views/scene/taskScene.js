d2adbe68b366e81f3749ddfb4b53eb96b497d8a0
mjm <mengjunmin@sina.cn>
2016-01-26 00:38:42 +0800

    任务

--- src/views/scene/taskScene.js
+++ src/views/scene/taskScene.js
@@ -5,15 +5,14 @@ var TaskScene = cc.Scene.extend({
 
     ctor: function (args) {
         this._super();
-
         cc.spriteFrameCache.addSpriteFrames(res.task_plist);
 
-        this.selected = 0;
+        this.selected = args.selected;
 
-        this.tabLayer = new TaskTabLayer({lobby: args.lobby, callback: this.onTabChange});
+        this.tabLayer = new TaskTabLayer({lobby: args.selected, callback: this.onTabChange});
         this.addChild(this.tabLayer, 9);
 
-        this.layer = new DailyTaskLayer();
+        this.layer = new DailyTaskLayer(args.data);
         this.addChild(this.layer);
 
     },
@@ -48,20 +47,41 @@ var TaskScene = cc.Scene.extend({
 
 });
 
+var CustomTableViewCell = cc.TableViewCell.extend({
+    draw:function (ctx) {
+        this._super(ctx);
+    }
+});
 
 var DailyTaskLayer = cc.Layer.extend({
     sprite:null,
-    ctor: function () {
+    ctor: function (args) {
         this._super();
+        this.data = args;
 
+        console.log('DailyTaskLayer event -> ', args);
         var winSize = cc.director.getWinSize();
+        var visibleOrigin = cc.director.getVisibleOrigin();
+        var visibleSize = cc.director.getVisibleSize();
 
-        //background
+ //background
         var bg = new cc.Sprite("#common_bg_beijing.png");
         bg.setPosition(winSize.width/2, winSize.height/2);
         bg.scale = ZGZ.SCALE * 10;
         this.addChild(bg);
-
+//
+        var taskList = args.taskList;
+        var cellH = 100;
+        var tabH = 80;
+//init data
+        this.m_pTableView = null;
+        this.m_nTableWidth = visibleSize.width;
+        this.m_nTableHeight = (cellH*taskList.length>(visibleSize.height-tabH)) ? (visibleSize.height-tabH):(cellH*taskList.length);
+        this.m_nCellWidth = visibleSize.width;
+        this.m_nCelleHeight = cellH;
+        this.m_nTableX = visibleOrigin.x;
+        this.m_nTableY = visibleOrigin.y + visibleSize.height - tabH - this.m_nTableHeight;
+        this.m_nCelleNum = taskList.length;
 
         this.init();
 
@@ -69,6 +89,70 @@ var DailyTaskLayer = cc.Layer.extend({
     init:function () {
         var winSize = cc.director.getWinSize();
 
+        this.m_pTableView = new cc.TableView(this, cc.size(this.m_nTableWidth, this.m_nTableHeight));
+        this.m_pTableView.setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL);
+        this.m_pTableView.x = this.m_nTableX;
+        this.m_pTableView.y = this.m_nTableY;
+        this.m_pTableView.setDelegate(this);
+        this.m_pTableView.setVerticalFillOrder(cc.TABLEVIEW_FILL_TOPDOWN);
+        this.addChild(this.m_pTableView);
+        this.m_pTableView.reloadData();
+
+
+    },
+
+    scrollViewDidScroll:function (view) {
+        //c.log("----->scrollViewDidScroll ");
+    },
+    scrollViewDidZoom:function (view) {
+        //cc.log("----->scrollViewDidZoom ");
+    },
+
+    tableCellTouched:function (table, cell) {
+        cc.log("cell touched at index: " + cell.getIdx());
+    },
+
+    tableCellSizeForIndex:function (table, idx) {
+        return cc.size(this.m_nCellWidth, this.m_nCelleHeight);
+    },
+
+    tableCellAtIndex:function (table, idx) {
+        var strValue = idx.toFixed(0);
+        var cell = table.dequeueCell();
+        var label;
+        if (!cell) {
+            cell = new CustomTableViewCell();
+        }
+        cell.removeAllChildren(true);
+        var taskList = this.data.taskList;
+
+        var desc = new cc.LabelTTF(taskList[idx].desc, "Arial", 24);
+        desc.color = cc.color.YELLOW;
+        desc.anchorX = 0;
+        desc.anchorY = 0.5;
+        desc.setPosition(10, this.m_nCelleHeight/2);
+        cell.addChild(desc);
+
+        var line = new cc.LabelTTF("------------------------------", "Arial", 24);
+        line.color = cc.color.YELLOW;
+        line.anchorX = 0.5;
+        line.anchorY = 0;
+        line.setPosition(this.m_nCellWidth/2, 0);
+        cell.addChild(line);
+
+        label = new cc.LabelTTF(strValue, "Helvetica", 20.0);
+        label.x = 0;
+        label.y = 0;
+        label.anchorX = 0;
+        label.anchorY = 0;
+        label.tag = 123;
+        cell.addChild(label);
+
+        return cell;
+    },
+
+    numberOfCellsInTableView:function (table) {
+        return this.m_nCelleNum;
     },
 
     onEnter: function () {
