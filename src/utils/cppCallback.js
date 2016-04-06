7ce48ba4ab528a5d128b4effb908e539fb23c4a2
yaoqiang <yaoqiangshmily@gmail.com>
2016-04-06 23:33:47 +0800

    add payment failed request for pingpp

--- src/utils/cppCallback.js
+++ src/utils/cppCallback.js
@@ -2,11 +2,11 @@
 // 定义全局函数用来处理C++回调
 //////////////////////////////////////////////
 
-//会出现闪退, 暂时不加了
+//
 function cppCallback_processPaidFailed(result) {
     //console.log('-------cppCallback_processPaidFailed-------');
     //console.log(result);
-    //UniversalController.payment4PingppFromClient(result, function () {
+    UniversalController.payment4PingppFromClient(result, function () {
     //    //console.log('payment4PingppFromClient finished..');
-    //});
+    });
 }
